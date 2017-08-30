25faccc52206c5ae11c4044c26c1a13b8773aadc
Kenneth Ho <kennethho@Kenneths-MacBook-Pro.local>
2017-08-29 20:57:50 -0500

    Test again

--- main.go
+++ main.go
@@ -9,5 +9,6 @@ func main() {
 	fmt.Println("Kenny test again, and matt test")
 	fmt.Println("Matt made a new Println")
 	fmt.Println("Kenny test again, and matt test, hi matt")
+	fmt.Println("Boike commit a test buddy guy pal")
 
 }
