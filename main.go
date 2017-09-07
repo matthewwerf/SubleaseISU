206e7e62ccdde2a75d52184f50cfe4058803152f
jboike <jboike@iastate.edu>
2017-09-07 14:13:20 -0500

    Added Angular to our project. Will Automatically run on LocalHost:4200

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
+	"net/http"
 )
 
 var boikeHang bool
@@ -22,6 +25,21 @@ func main() {
 
 	hiKenny(1)
 	JackTempFunction("*")
+
+	port := flag.Int("port", 4200, "port to serve on")
+	dir := flag.String("directory", "web/", "directory of web files")
+	flag.Parse()
+
+	fs := http.Dir(*dir)
+	fileHandler := http.FileServer(fs)
+	http.Handle("/", fileHandler)
+
+	log.Printf("Running on port %d\n", *port)
+	addr := fmt.Sprintf("127.0.0.1:%d", *port)
+	// this is a blocking call, the program will run here forever
+	err := http.ListenAndServe(addr, nil)
+	fmt.Println(err.Error())
+
 }
 
 func hiKenny(temp int) {
