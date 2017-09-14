f391c1c916719fa13e2acd5f1d9b58fb31cad165
jboike <jboike@iastate.edu>
2017-09-14 13:32:34 -0500

    Created the Main homepage for our app, along with the router to route all the requests to different pages.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { MyNewComponentComponent } from './my-new-component/my-new-component.component';
+import { HomeComponent } from './home/home.component';
+
+
+// This is the router module where you add routes to components (Pages)
+export const ROUTES: Routes = [
+	{ path: '', component: HomeComponent}
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyNewComponentComponent
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
