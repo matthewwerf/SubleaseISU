8a235bcd7f5381d2431a84ab7d778f9ab4326fd4
Leonard <ef63577@Oaksal201.local>
2017-09-24 19:47:06 -0500

    Log In now creates user model

--- src/app/log-in-component/log-in-component.component.ts
+++ src/app/log-in-component/log-in-component.component.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+import { User } from '../User';
 
 @Component({
   selector: 'app-log-in-component',
   templateUrl: './log-in-component.component.html',
   styleUrls: ['./log-in-component.component.css']
 })
-export class LogInComponentComponent {
+export class LogInComponentComponent implements OnInit{
 	
+	logInForm: FormGroup;
 
-	onSubmit(form: any): void{
-		console.log('User Info:', form);
-	}
+  ngOnInit() {
+      this.logInForm = new FormGroup ({
+      username: new FormControl(),
+      password: new FormControl(),
+    });
+  }
 }
