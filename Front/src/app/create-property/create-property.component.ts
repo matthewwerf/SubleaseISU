0f4c4263e37c3b78e2d2e14c1b0661760db15d8c
Kenneth Ho <kennyho1516@gmail.com>
2017-09-27 21:57:23 -0500

    Created new property page. Json value is created on submit, ready to be sent to backend

--- Front/src/app/create-property/create-property.component.ts
+++ Front/src/app/create-property/create-property.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
+
+
 @Component({
   selector: 'app-create-property',
   templateUrl: './create-property.component.html',
@@ -8,58 +10,87 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class CreatePropertyComponent implements OnInit {
 
-  	constructor() { }
+    //constructor() { }
 
-  	newPropertyForm: FormGroup;
-  	posterUsername: FormControl;
-	leasingAgency: FormControl;
-	rentValue: FormControl;
-	address: FormControl;
-	postingMessage: FormControl;
-	linkedPictureIDs: FormControl;
-	propertyID: FormControl;
+    newPropertyForm: FormGroup;
+    posterUsername: FormControl;
+    leasingAgency: FormControl;
+    rentValue: FormControl;
+    address: FormControl;
+    postingMessage: FormControl;
+    linkedPictureIDs: FormControl;
+    propertyID: FormControl;
+    errorMessage: string; 
 
-  	ngOnInit() {
-  		this.createFormControls();
-  		this.createForm();
+    ngOnInit() {
+      this.createFormControls();
+      this.getPosterUsername();
+      this.createPicures();
+      this.createPropertyID();
+      this.createForm();
   }
 
   createFormControls() {
-  	this.posterUsername = new FormControl('', Validators.required);
-  	this.leasingAgency = new FormControl('', Validators.required);
-  	this.rentValue = new FormControl('', [Validators.required, Validators.pattern("[0-9]*")]);
-  	this.address = new FormControl('', Validators.required);
-  	this.postingMessage = new FormControl();
-  	
+    //this.posterUsername = new FormControl('', Validators.required);
+    this.address = new FormControl('', Validators.required);
+    this.leasingAgency = new FormControl('', Validators.required);
+    this.rentValue = new FormControl('', Validators.required);//[Validators.required, Validators.pattern("[0-9]*")]);
+    this.postingMessage = new FormControl();
+    
 
   }
 
+  getPosterUsername() {
+    //Retrieve poster's username via http GET method
+    this.posterUsername = new FormControl();
+  }
   createPicures() {
-  	//Figure out how to upload images
-  	//this.linkedPictureIDs = new FormControl();
+    //Figure out how to upload images
+    this.linkedPictureIDs = new FormControl();
   }
 
   createPropertyID() {
-  	//Figure out how to generate unique propertyID
-
+    //Figure out how to generate unique propertyID
+    this.propertyID = new FormControl();
   }
 
   createForm() {
-  	this.newPropertyForm = new FormGroup ({
-  		posterUsername: this.posterUsername,
-		leasingAgency: this.leasingAgency,
-		rentValue: this.rentValue,
-		address: this.address,
-		postingMessage: this.postingMessage,
-		linkedPictureIDs: this.linkedPictureIDs,
-		propertyID: this.propertyID
-  	});
+    this.newPropertyForm = new FormGroup ({
+        posterUsername: this.posterUsername,
+        address: this.address,
+        leasingAgency: this.leasingAgency,
+        rentValue: this.rentValue,
+        postingMessage: this.postingMessage,
+        linkedPictureIDs: this.linkedPictureIDs,
+        propertyID: this.propertyID
+    });
   }
-  onsubmit() {
-  	if (this.newPropertyForm.valid) {
-  		console.log("New Property Request Submitted");
-  		this.newPropertyForm.reset();
-  		//send json to server via http
-  	}
+  onSubmit() {
+    if (this.newPropertyForm.valid) {
+      console.log("New Property Request Submitted");
+      console.log(this.newPropertyForm.value);
+      this.newPropertyForm.reset();
+      this.errorMessage = "";
+      document.getElementById("errorMsg").innerText = this.errorMessage;
+      //send json to server via http POST method
+    }
+    else {
+      this.errorMessage = "Invalid entries at: \n";
+      
+      if(this.address.invalid) {
+        this.errorMessage = this.errorMessage + "*Address \n";
+      }
+      if(this.leasingAgency.invalid) {
+        this.errorMessage = this.errorMessage + "*Leasing Agency \n";
+      }
+      if(this.rentValue.invalid) {
+        this.errorMessage = this.errorMessage + "*Rent Value \n";
+      }
+      if(this.postingMessage.invalid) {
+        this.errorMessage = this.errorMessage + "*Property Description \n";
+      }
+      document.getElementById("errorMsg").innerText = this.errorMessage;
+      
+    }
   }
 }
