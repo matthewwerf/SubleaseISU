591325d00e48387de1e39d3a39d92d3beda93f17
Kenneth Ho <kennyho1516@gmail.com>
2017-10-03 16:55:34 -0500

    Adding structured address input to standardize address value

--- Front/src/app/create-property/create-property.component.ts
+++ Front/src/app/create-property/create-property.component.ts
@@ -10,34 +10,55 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class CreatePropertyComponent implements OnInit {
 
-    //constructor() { }
+    constructor() { }
 
     newPropertyForm: FormGroup;
+    newAddressForm: FormGroup;
     posterUsername: FormControl;
     leasingAgency: FormControl;
     rentValue: FormControl;
     address: FormControl;
+    streetAddress: FormControl;
+    city: FormControl;
+    state: FormControl;
+    zip: FormControl;
     postingMessage: FormControl;
     linkedPictureIDs: FormControl;
     propertyID: FormControl;
-    errorMessage: string; 
+    errorMessage: string;
+    testStr: string;
+    addressValue: string; 
 
     ngOnInit() {
       this.createFormControls();
+      this.getAddress();
       this.getPosterUsername();
       this.createPicures();
       this.createPropertyID();
+      this.createAddressForm();
       this.createForm();
   }
 
   createFormControls() {
     //this.posterUsername = new FormControl('', Validators.required);
-    this.address = new FormControl('', Validators.required);
+    //this.address = new FormControl('', Validators.required);
+    this.streetAddress = new FormControl('', Validators.required);
+    //console.log(this.streetAddress.value);
+    this.city = new FormControl('', Validators.required);
+    this.state = new FormControl('', Validators.required);
+    this.zip = new FormControl('', Validators.required);
     this.leasingAgency = new FormControl('', Validators.required);
     this.rentValue = new FormControl('', Validators.required);//[Validators.required, Validators.pattern("[0-9]*")]);
     this.postingMessage = new FormControl();
     
 
+  }
+
+  getAddress(){
+    this.address = new FormControl();
+    this.addressValue = this.streetAddress.value + ", " + this.city.value + ", " + this.state.value + " " + this.zip.value
+    console.log(this.streetAddress.value);
+    this.address.setValue(this.addressValue);
   }
 
   getPosterUsername() {
@@ -54,21 +75,32 @@ export class CreatePropertyComponent implements OnInit {
     this.propertyID = new FormControl();
   }
 
+
+  createAddressForm(){
+    this.newAddressForm = new FormGroup ({
+      streetAddress: this.streetAddress,
+      city: this.city,
+      state: this.state,
+      zip: this.zip
+    });
+  }
+
   createForm() {
     this.newPropertyForm = new FormGroup ({
-        posterUsername: this.posterUsername,
-        address: this.address,
-        leasingAgency: this.leasingAgency,
-        rentValue: this.rentValue,
-        postingMessage: this.postingMessage,
-        linkedPictureIDs: this.linkedPictureIDs,
-        propertyID: this.propertyID
+      posterUsername: this.posterUsername,
+      address: this.address,
+      leasingAgency: this.leasingAgency,
+      rentValue: this.rentValue,
+      postingMessage: this.postingMessage,
+      linkedPictureIDs: this.linkedPictureIDs,
+      propertyID: this.propertyID
     });
   }
   onSubmit() {
     if (this.newPropertyForm.valid) {
       console.log("New Property Request Submitted");
       console.log(this.newPropertyForm.value);
+      console.log(this.streetAddress.value);
       this.newPropertyForm.reset();
       this.errorMessage = "";
       document.getElementById("errorMsg").innerText = this.errorMessage;
