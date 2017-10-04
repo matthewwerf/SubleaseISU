f688158ba3e819405e91050dbc165efa7479003e
Matthew Vanderwerf <matthewwerf@gmail.com>
2017-10-04 16:28:39 -0500

    Property Routes with authentication

--- REST/controllers/propertyController.js
+++ REST/controllers/propertyController.js
@@ -1,54 +1,123 @@
 (function () {'use strict';
 
 	var mongoose = require("mongoose"),
-		Property = mongoose.model('Property');
-	var sha1 = require("sha1");
-	var config = require("../config.js");
+		Property = mongoose.model('Property'),
+		User = mongoose.model('User'),
+		sha1 = require("sha1"),
+		config = require("../config.js");
 
 	exports.createProperty = function(req, res) {
-		if (req.body.username && req.body.propertyID){
+		if (!req.body.subleaseISUcookie || !req.body.username){
+			res.status(401).send({
+				"error": "not authenticated"
+			});
+			return;
+		} else {
+			User.findOne({username: req.body.username}, 'hashedPassword', function(err, user){
+				localCookieToCheck = sha1(req.body.username + user.hashedPassword + config.salt);
+				if(localCookieToCheck != req.body.subleaseISUcookie) {
+					res.status(401).send({
+						"error": "authentication rejected"
+					});
+					return;
+				}
+			});
+		}
+
+		if (req.body.propertyID){
 
 			var newProperty = new Property(req.body);
 
 			newProperty.save(function (err, property) {
 				if (err) {
-					res.send(err);
+					res.status(500).send(err);
 				}
-				res.json(property);
+				res.status(201).json(property);
 			});
 		} else {
-			res.send({
+			res.status(400).send({
 				"error": "username or propertyID missing"
 			});
 		}
 	};
 
 	exports.getSpecificProperty = function(req, res) {
+		if (!req.body.subleaseISUcookie || !req.body.username){
+			res.status(401).send({
+				"error": "not authenticated"
+			});
+			return;
+		} else {
+			User.findOne({username: req.body.username}, 'hashedPassword', function(err, user){
+				localCookieToCheck = sha1(req.body.username + user.hashedPassword + config.salt);
+				if(localCookieToCheck != req.body.subleaseISUcookie) {
+					res.status(401).send({
+						"error": "authentication rejected"
+					});
+					return;
+				}
+			});
+		}
+
 		Property.findOne({propertyID: req.params.propertyID}, function (err, property) {
 			if (err) {
-				res.send(err);
+				res.status(500).send(err);
 			}
-			res.json(property);
+			res.status(200).json(property);
 		});
 	};
 
 	exports.updateSpecificProperty = function(req, res) {
+		if (!req.body.subleaseISUcookie || !req.body.username){
+			res.status(401).send({
+				"error": "not authenticated"
+			});
+			return;
+		} else {
+			User.findOne({username: req.body.username}, 'hashedPassword', function(err, user){
+				localCookieToCheck = sha1(req.body.username + user.hashedPassword + config.salt);
+				if(localCookieToCheck != req.body.subleaseISUcookie) {
+					res.status(401).send({
+						"error": "authentication rejected"
+					});
+					return;
+				}
+			});
+		}
+
 		Property.findOneAndUpdate({propertyID: req.params.propertyID}, req.body, {new: true}, function (err, property){
 			if (err) {
-				res.send(err);
+				res.status(500).send(err);
 			}
-			res.json(property);
+			res.status(200).json(property);
 		});
 	};
 
 	exports.deleteSpecificProperty = function(req, res){
+		if (!req.body.subleaseISUcookie || !req.body.username){
+			res.status(401).send({
+				"error": "not authenticated"
+			});
+			return;
+		} else {
+			User.findOne({username: req.body.username}, 'hashedPassword', function(err, user){
+				localCookieToCheck = sha1(req.body.username + user.hashedPassword + config.salt);
+				if(localCookieToCheck != req.body.subleaseISUcookie) {
+					res.status(401).send({
+						"error": "authentication rejected"
+					});
+					return;
+				}
+			});
+		}
+		
 		Property.remove({
 			propertyID: req.params.propertyID
 		}, function(err, property){
 			if (err) {
-				res.send(err);
+				res.status(500).send(err);
 			}
-			res.json({message: 'Property successfully deleted'});
+			res.status(200).json({message: 'Property successfully deleted'});
 		});
 	};
 
