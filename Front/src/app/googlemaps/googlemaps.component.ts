31d7548ff27b09022c0320455f781aa9ddb35323
Leonard <ef63577@oaksal201.student.iastate.edu>
2017-10-04 16:51:38 -0500

    more changes to maps

--- Front/src/app/googlemaps/googlemaps.component.ts
+++ Front/src/app/googlemaps/googlemaps.component.ts
@@ -9,9 +9,17 @@ export class GooglemapsComponent implements OnInit {
 
   constructor() { }
 
-  
-
   title: string = 'Google Maps';
+/*
+  let list = [135 campus ave, 212 welch ave, 301 Tripp St]
+
+  for(let i in list){
+  	lat: number = 
+  	lng: number = 
+  }
+
+  */
+
   lat: number = 42.0308;
   lng: number = -93.6319;
 
