6093f3d6bfd37b84d0dacb3bfc8471f8afd7ff48
Leonard <ef63577@oaksal201.student.iastate.edu>
2017-10-04 14:28:54 -0500

    Reverted Google Maps to AGMlibrary

--- Front/src/app/googlemaps/googlemaps.component.ts
+++ Front/src/app/googlemaps/googlemaps.component.ts
@@ -9,10 +9,11 @@ export class GooglemapsComponent implements OnInit {
 
   constructor() { }
 
+  
+
   title: string = 'Google Maps';
   lat: number = 42.0308;
   lng: number = -93.6319;
-
 
   ngOnInit() {
   }
