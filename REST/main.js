87d8175dc6ee649ac0ecd1ff245964fea1c7f1a2
matthewv <matthewv@iastate.edu>
2017-10-06 15:18:13 -0500

    Allow CORS

--- REST/main.js
+++ REST/main.js
@@ -19,6 +19,7 @@ var propertyRoutes = require('./routes/propertyRoutes');
 propertyRoutes(app);
 
 app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
