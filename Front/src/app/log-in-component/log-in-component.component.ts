d4c610947f5e9ecc7cc56d70e586cb3c8e375b0d
jboike <jboike@iastate.edu>
2017-10-06 15:06:31 -0500

    Log-in page now posts to the URL and hashes the password.

--- Front/src/app/log-in-component/log-in-component.component.ts
+++ Front/src/app/log-in-component/log-in-component.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
 import { User } from '../models/user';
+
+import * as crypto from 'crypto-js';
 
 @Component({
   selector: 'app-log-in-component',
@@ -8,17 +13,61 @@ import { User } from '../models/user';
   styleUrls: ['./log-in-component.component.css']
 })
 export class LogInComponentComponent {
+
+  signInForm: FormGroup;
+  Username: FormControl;
+  Password: FormControl;
+
 	user: User = new User();
-	constructor(private auth: AuthService) {
+	constructor(private auth: AuthService, private http: HttpClient) {
 
 	}
 
+  ngOnInit() {
+    this.createFormControls();
+    this.createForm();
+  }
+
+  createFormControls() {
+    this.Username = new FormControl('', Validators.required);
+    this.Password = new FormControl('', Validators.required);
+  }
+
+  createForm() {
+    this.signInForm = new FormGroup ({
+      Username: this.Username,
+      Password: this.Password,
+     });
+  }
+
 	onSubmit(form: any): void{
-    	this.auth.login(this.user).then((user) => {
-    		localStorage.setItem('token', user.json().auth_token);
-      		console.log(user.json());
-    		}).catch((err) => {
-      			console.log(err);
-    		});
+    if (this.signInForm.valid) {
+
+    // Hash the password with SHA1
+        var hashedPassword = crypto.SHA1(this.Password);
+        
+        // POST the user to the backend
+        const req = this.http.post('http://jsonplaceholder.typicode.com/posts', {
+          username: this.Username.value,
+          password: hashedPassword.toString(),
+        }).subscribe(
+            res => {
+              console.log(res);
+            },
+            err => {
+              console.log(err);
+            }
+        );
+
+
+
+
+    	// this.auth.login(this.user).then((user) => {
+    	// 	localStorage.setItem('token', user.json().auth_token);
+     //  		console.log(user.json());
+    	// 	}).catch((err) => {
+     //  			console.log(err);
+    	// 	});
+    }
 		}
 }
