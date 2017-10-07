bf42896e906c75041973cb218f3b9ee0ae8398d2
Matthew Vanderwerf <matthewwerf@gmail.com>
2017-10-07 16:21:57 -0500

    Try this maybe

--- REST/routes/userRoutes.js
+++ REST/routes/userRoutes.js
@@ -3,6 +3,13 @@
 	module.exports = function(app) {
 		var user = require("../controllers/userController");
 
+		app.use(function(req, res, next) {
+			res.header("Access-Control-Allow-Origin", "*");
+			res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+			res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+		  next();
+		});
+
 		app.route('/users')
 			.post(user.createUser);
 
