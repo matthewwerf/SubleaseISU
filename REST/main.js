6366573fd4f4e6e01b2b707d9b141a8ab7aa730e
Matthew Vanderwerf <matthewwerf@gmail.com>
2017-10-07 16:16:50 -0500

    Switching order to attempt to allow CORS

--- REST/main.js
+++ REST/main.js
@@ -12,18 +12,18 @@ mongoose.connect('mongodb://127.0.0.1:27017/SubleaseISU');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  next();
+});
+
 //var routes = require('./routes/userRoutes')(app);
 var userRoutes = require('./routes/userRoutes'); //importing route
 userRoutes(app);
 var propertyRoutes = require('./routes/propertyRoutes');
 propertyRoutes(app);
-
-app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 app.listen(port);
 
