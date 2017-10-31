651ea27ce789e02771c9cab37df777f65be1f502
jboike <jboike@iastate.edu>
2017-10-31 16:56:19 -0500

    Changed the listing object to fit our requirements better.

--- Front/src/app/models/listing.ts
+++ Front/src/app/models/listing.ts
@@ -1,8 +1,19 @@
 export class ListingInfo {
-	address: String;
-	price: number;
-	description: string;
+	_id: string;
 	posterUsername: string;
-	propertyID: string;
 	leasingAgency: string;
+	rentValue: number;
+	address: string;
+	postingMessage: string;
+	propertyId: string;
+
+	constructor(id: string, posterUsername: string, leasingAgency: string, rentValue: number, address: string, postingMessage: string, propertyId: string) {
+		this._id = id;
+		this.posterUsername = posterUsername;
+		this.leasingAgency = leasingAgency;
+		this.rentValue = rentValue;
+		this.address = address;
+		this.postingMessage = postingMessage;
+		this.propertyId = propertyId;
+	}
 }
