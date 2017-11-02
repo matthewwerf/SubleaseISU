735335ca7178390619c66fff897277228b1bf8a9
Kenneth Ho <kennyho1516@gmail.com>
2017-11-01 22:51:06 -0500

    Still working on picture uploading

--- Front/src/app/view-profile/view-profile.component.ts
+++ Front/src/app/view-profile/view-profile.component.ts
@@ -12,13 +12,39 @@ export class ViewProfileComponent implements OnInit {
 
   constructor() { }
   public uploader: FileUploader = new FileUploader({url: URL, itemAlias: 'newProfilePicture'});
-  title: string
+  title: string;
+  previewImage: any;
+  tempImage: any;
+  source: string;
+
+  imagePreview(input) 
+  {
+      document.getElementById("previewImage").style.display="block";
+      
+        console.log("Image is selected")
+        this.previewImage = document.getElementById("previewImage");
+        console.log(this.previewImage);
+        
+        this.source = (<HTMLImageElement>document.getElementById('previewImage')).src
+        console.log("Image Source: " + this.source + " Should be inbetween here");
+        
+      
+      //var reader = new FileReader();
+        
+        //console.log(this.previewImage);
+      
+  }
+
   ngOnInit() {
   	this.title = 'View or Update Your Profile';
   	this.uploader.onAfterAddingFile = (file)=> {file.withCredentials = false;};
   	this.uploader.onCompleteItem = (item:any, response:any, status:any, headers:any) => {
         console.log("ImageUpload:uploaded:", item, status, response);
+    //this.imagePreview();
+
+
     };
   }
+
 
 }
