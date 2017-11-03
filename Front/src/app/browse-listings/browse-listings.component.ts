02191394d4d7032ccf4e4219ec8a9076bb196c20
Jack <jackb032@gmail.com>
2017-11-03 14:33:47 -0500

    Getting an array of listings from the DB now works correctly.
    HTML is still incorrect.

--- Front/src/app/browse-listings/browse-listings.component.ts
+++ Front/src/app/browse-listings/browse-listings.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ListingInfo } from '../models/listing';
+import { Observable } from 'rxjs';
 import { BrowseListingsService } from './browse-listings-service';
 
 @Component({
@@ -10,16 +11,17 @@ import { BrowseListingsService } from './browse-listings-service';
 
 export class BrowseListingsComponent implements OnInit {
 
-	private ListingList: ListingInfo[] = [];
+	private ListingList: ListingInfo[];
+  private isLoaded: boolean;
 
   constructor(private browseListingsService: BrowseListingsService) { }
 
   ngOnInit() {
-  	this.getListings();
-  }
+    this.isLoaded = false;
+    this.browseListingsService.getListings().subscribe( ListingInfo => { 
+      this.ListingList = ListingInfo
+      this.isLoaded = true;
 
-  getListings() {
-  	this.browseListingsService.getListings().subscribe( ListingInfo => { this.ListingList = ListingInfo});
+    });
   }
-
 }
