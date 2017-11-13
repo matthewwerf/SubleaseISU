f8131d03e865ff8818f174cd5772a0ee51477fd5
Matthew Vanderwerf <matthewwerf@gmail.com>
2017-11-13 16:21:36 -0600

    Start shell of unit testing

---
+++ REST/test/userTests.js
@@ -0,0 +1,22 @@
+var chai = require('chai'),
+	assert = chai.assert;
+
+describe('createUser Function', function(){
+	it('Should Catch Database Error', function() {
+		
+	});
+
+	it('Should Catch Null User', function() {
+
+	});
+
+	it('Should Send Proper Success Response', function() {
+
+	});
+
+	it('Should Error Out On Same User Name', function() {
+
+	});
+
+	
+});
