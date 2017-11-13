ced3e04c43425b85e137025945604d99fbc5e8f2
Matthew Vanderwerf <matthewwerf@gmail.com>
2017-11-13 17:41:45 -0600

    More test cases

--- REST/test/userTests.js
+++ REST/test/userTests.js
@@ -35,6 +35,27 @@ describe('Users', () => {
 		});
 	});
 
+	describe('GET existing User', () => {
+		it('Should retrieve valid user', (done) =>{
+			let newUser = {
+				username: "username",
+				hashedPassword: "hashedPassword"
+			};
+			newUser = new User(newUser);
+			newUser.save((err, user) => {
+				chai.request(server)
+					.get('/users/username')
+					.end((err, res) => {
+						res.should.have.status(200);
+						res.body.should.be.a('object');
+						res.body.should.have.property('username');
+						res.body.should.have.property('hashedPassword');
+						done();
+				});
+			});
+		});
+	});
+
 	describe('POST new User', () => {
 		it('Should create user with all reqs', (done) => {
 			let newUser = {
@@ -54,5 +75,61 @@ describe('Users', () => {
 				});
 		});
 	});
+
+	describe('Auth Existing User', () => {
+		it('Should return cookie on valid auth', (done) => {
+			let newUser = {
+				username: "username",
+				hashedPassword: "hashedPassword"
+			};
+			newUser = new User(newUser);
+			newUser.save((err, user) => {
+				chai.request(server)
+					.post('/login/username')
+					.send({
+						hashedPassword: "hashedPassword",
+						username: "username"
+					})
+					.end((err, res) => {
+						res.should.have.status(200);
+						res.body.should.be.a('object');
+						res.body.should.have.property('subleaseISUcookie');
+						done();
+				});
+			});
+		});
+	});
+
+	describe('PUT existing User', () => {
+		it('Should update valid user', (done) =>{
+			let newUser = {
+				username: "username",
+				hashedPassword: "hashedPassword"
+			};
+			newUser = new User(newUser);
+			newUser.save((err, user) => {
+				chai.request(server)
+					.put('/users/username')
+					.send({
+						hashedPassword: "newHashedPassword",
+						username: "username",
+						subleaseISUcookie: "f069d9f42153600e1ad8d8106aa12411760ae79c"
+					})
+					.end((err, res) => {
+						res.should.have.status(200);
+						res.body.should.be.a('object');
+						res.body.should.have.property('username');
+						res.body.should.have.property('hashedPassword');
+						res.body.hashedPassword.should.be.eql('newHashedPassword');
+						done();
+				});
+			});
+		});
+	});
+
+
+
+
+
 
 });
