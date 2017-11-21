6381bab8cc14a7af0df24b20df9de94fe16e9820
matthewv <matthewwerf@gmail.com>
2017-11-21 16:18:42 -0600

    Correcting profile picture serving

--- REST/controllers/userController.js
+++ REST/controllers/userController.js
@@ -223,9 +223,10 @@
 	exports.retrieveProfilePic = function(req, res) {
 		ah.validateAuth(req, res, function(user) {
 			if(user != null) {
+				console.log(user);
 				if(user.profilePictureLocation != null) {
-					let path = user.profilePictureLocation.replace(/(\s+)/g, '\\$1');
-					res.status(200).sendFile(path);
+					//let path = user.profilePictureLocation.replace(/(\s+)/g, '\\$1');
+					res.status(200).sendFile(user.profilePictureLocation);
 				} else {
 					res.status(404).send({
 						"error": "Profile Picture Location Does Not Exist"
