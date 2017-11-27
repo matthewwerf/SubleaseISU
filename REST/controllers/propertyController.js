6dbc66b29e69ad98ecf625faa3b8058ef2c8f5fe
Matthew Vanderwerf <matthewwerf@gmail.com>
2017-11-27 14:49:29 -0600

    Change format of date time on comments

--- REST/controllers/propertyController.js
+++ REST/controllers/propertyController.js
@@ -278,7 +278,7 @@
 
 					var newComment = {
 						commentPosterUsername: req.body.username,
-						timePosted: Date.now(),
+						timePosted: getDateTime(),
 						message: req.body.message
 					};
 
@@ -299,7 +299,30 @@
 		});
 	};
 
+	function getDateTime() {
 
+	    var date = new Date();
+
+	    var hour = date.getHours();
+	    hour = (hour < 10 ? "0" : "") + hour;
+
+	    var min  = date.getMinutes();
+	    min = (min < 10 ? "0" : "") + min;
+
+	    var sec  = date.getSeconds();
+	    sec = (sec < 10 ? "0" : "") + sec;
+
+	    var year = date.getFullYear();
+
+	    var month = date.getMonth() + 1;
+	    month = (month < 10 ? "0" : "") + month;
+
+	    var day  = date.getDate();
+	    day = (day < 10 ? "0" : "") + day;
+
+	    return year + ":" + month + ":" + day + ":" + hour + ":" + min + ":" + sec;
+
+	}
 
 
 
