57721b60758b27afbb7b8ea4f793ee943a79c934
Matthew Vanderwerf <matthewwerf@gmail.com>
2017-12-03 17:19:29 -0600

    Adding admin and leasing auth

--- REST/lib/authHelper.js
+++ REST/lib/authHelper.js
@@ -9,7 +9,7 @@
 	 * Returns null on error, user on success
 	 */
 	exports.validateAuth = function(req, res, cb) {
-		var queryUsername;
+	var queryUsername;
 		if(req.params.username != null) {
 			queryUsername = req.params.username;
 		} else if(req.body.username != null) {
@@ -36,7 +36,36 @@
 			}
 			var localCookieToCheck = sha1(queryUsername + user.hashedPassword + config.salt);
 			if (localCookieToCheck == req.body.subleaseISUcookie){
-				cb(user);
+				if(user.userType) {
+					if(user.userType == 'admin') {
+						if(user.userTypeApproved == true) {
+							handleAdmin(req, res, function(adminAccount){
+								cb(adminAccount);
+							});
+						} else {
+							res.status(400).json({
+								"error" : "account is still pending approval for admin status"
+							});
+							cb(null);
+							return;
+						}
+						
+					} else if(user.userType == 'leasing') {
+						if(user.userTypeApproved == true) {
+							handleLeaser(req, res, function(leaserAccount) {
+								cb(leaserAccount);
+							});
+						} else {
+							res.status(400).json({
+								"error" : "account is still pending approval for leasing status"
+							});
+							cb(null);
+							return;
+						}
+					}
+				} else {
+					cb(user);
+				}
 			} else {
 				res.status(400).json({
 					"error": "Incorrect cookie"
@@ -45,5 +74,13 @@
 			}
 		});
 	};
+
+	function handleAdmin(req, res, adminAccount, cb) {
+		cb(adminAccount);
+	}
+
+	function handleLeaser(req, res, leasingAccount, cb) {
+		cb(leasingAccount);
+	}
 
 }());
