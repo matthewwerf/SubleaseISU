2b4c1ce6f4a9e4a8fd9e9773327de043b3cc9462
Jack <jackb032@gmail.com>
2017-12-03 12:33:22 -0600

    Admin approvate page start

---
+++ Front/src/app/admin-approval/admin-approval.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-approval',
+  templateUrl: './admin-approval.component.html',
+  styleUrls: ['./admin-approval.component.css']
+})
+export class AdminApprovalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
